4ceb7b035f99ebcebcb97c78e3f7044805dbef21
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-03-03 00:24:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to MailMind</h1>
-    </div>
-  );
-}
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello MailMind!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
